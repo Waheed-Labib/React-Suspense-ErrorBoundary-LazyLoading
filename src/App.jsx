d629cec31dae5cc6657fd5d346fb68f64c1ca8c2
Waheed-Labib <labibwahid28@gmail.com>
2025-01-02 22:19:46 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <ol>
+        <h1>Different fetching approaches with React</h1>
+        <li>
+          <h2>Fetch on Render</h2>
+        </li>
+
+        <li>
+          <h2>Fetch then Render</h2>
+        </li>
+
+        <li>
+          <h2>Render while Fetching</h2>
+        </li>
+      </ol>
+    </>
+  )
+}
+
+export default App
